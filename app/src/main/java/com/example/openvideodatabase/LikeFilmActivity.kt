5b94f2f807cd80350115913357e19eb96c58fb8d
Davide <dvdbrugnoli@gmail.com>
2025-08-11 19:25:14 +0200

    implementazione menu cliccabili

--- app/src/main/java/com/example/openvideodatabase/LikeFilmActivity.kt
+++ app/src/main/java/com/example/openvideodatabase/LikeFilmActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import android.content.Intent
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
 import com.example.openvideodatabase.data.ReviewRepository
 import com.example.openvideodatabase.data.local.AppDatabase
 import com.example.openvideodatabase.data.local.Review
@@ -47,13 +49,12 @@ fun FavoritesScreen(reviewRepository: ReviewRepository, onBack: () -> Unit) {
     var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
     var allFavorites by remember { mutableStateOf(listOf<Review>()) }
     var filteredFavorites by remember { mutableStateOf(listOf<Review>()) }
-
     val coroutineScope = rememberCoroutineScope()
 
-    // Carica tutti i preferiti dal DB quando la schermata si apre
+
     LaunchedEffect(Unit) {
         coroutineScope.launch(Dispatchers.IO) {
-            val favorites = reviewRepository.getAllReviews() // Devi avere questa funzione nel DAO
+            val favorites = reviewRepository.getAllReviews()
             withContext(Dispatchers.Main) {
                 allFavorites = favorites
                 filteredFavorites = favorites
@@ -110,16 +111,24 @@ fun FavoritesScreen(reviewRepository: ReviewRepository, onBack: () -> Unit) {
 
 @Composable
 fun FavoriteItem(review: Review) {
+    val context = LocalContext.current
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp),
+            .padding(vertical = 4.dp)
+            .clickable {
+                val intent = Intent(context, ShowDettailActivity::class.java).apply {
+                    putExtra("imdbID", review.externalId)
+                }
+                context.startActivity(intent)
+            },
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = review.title, style = MaterialTheme.typography.titleMedium)
             if (!review.comment.isNullOrEmpty()) {
-                Text(text = review.comment!!, style = MaterialTheme.typography.bodyMedium)
+                Text(text = review.comment, style = MaterialTheme.typography.bodyMedium)
             }
         }
     }
